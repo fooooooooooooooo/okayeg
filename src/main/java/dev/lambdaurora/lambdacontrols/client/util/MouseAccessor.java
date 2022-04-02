b736f6d88876215a16f401a99fccd61f34670128
Ty <tyjupiter9@gmail.com>
2022-04-01 20:50:39 -0400

    hello

---
+++ src/main/java/dev/lambdaurora/lambdacontrols/client/util/MouseAccessor.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright � 2021 LambdAurora <aurora42lambda@gmail.com>
+ *
+ * This file is part of LambdaControls.
+ *
+ * Licensed under the MIT license. For more information,
+ * see the LICENSE file.
+ */
+
+package dev.lambdaurora.lambdacontrols.client.util;
+
+/**
+ * Represents mouse's extra access.
+ */
+public interface MouseAccessor {
+    void lambdacontrols$onCursorPos(long window, double x, double y);
+}
